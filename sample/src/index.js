d936e205366fea8961b33cc2bc749ec11c17d785
Daym3l <daym3l@nauta.cu>
2019-07-09 09:37:14 -0400

    style update

--- sample/src/index.js
+++ sample/src/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import { render } from 'react-dom';
 import GalleryPicker from '../../src/index';
 
-const IMG1 = "public/assets/images/1.jpg"
+const IMG1 = "public/assets/images/1.jpg";
+const IMG2 = "public/assets/images/2.jpg";
+const IMG3 = "public/assets/images/3.jpg";
 
 const App = () => {
     const getImages = images => {
         console.log(images);
     }
-    const imageList = [IMG1]
+    const imageList = [IMG1, IMG2, IMG3];
 
     return <GalleryPicker imagesRecived={imageList} returnImages={getImages} />;
 };
 
 
-render(<App/>, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
